3986975b4c26488ce4903f667d65dd8c82079a0c
Philipp <filipp.ryabchinski@gmail.com>
2025-11-11 13:40:03 +0300

    added classes for EntityEmoji.java and for Coordinates.java

---
+++ src/main/java/world/Coordinates.java
@@ -0,0 +1,43 @@
+package world;
+
+
+public class Coordinates {
+    private int x;
+    private int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+}
